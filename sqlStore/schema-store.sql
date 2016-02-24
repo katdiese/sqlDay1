8b9e8540dbb667af1135135ba67d9837f30babdf
Kathy Dieser <katdiese@gmail.com>
2016-02-23 17:12:54 -0700

    added more sql statements

--- sqlStore/schema-store.sql
+++ sqlStore/schema-store.sql
@@ -20,3 +20,9 @@ CREATE TABLE products(
   image_url text,
   store_id integer REFERENCES stores(id)
 );
+
+INSERT INTO stores(id, name, location, web_address, category) VALUES (default, 'Target', '1234 Somewhere Dr.', 'www.target.com', 'everything');
+
+INSERT INTO products(id, price, barcode, name, description, image_url, store_id) VALUES
+  (default, 44.21, '000001234', 'Shampoo', '44oz bottle', 'www.google.com', 1),
+  (default, 44.21, '000001234', 'Shampoo', '44oz bottle', 'www.google.com', 1);
